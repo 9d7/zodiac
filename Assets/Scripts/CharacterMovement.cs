fd97cc1fd75386d2c16a6c4f6c416818582bf538
TianjieJia <tjia@purdue.edu>
2021-03-19 13:34:11 -0400

    Jerry complete camera control and hanging bridge

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.WSA;
-using UnityEngine.SceneManagement;
+
 
 public class CharacterMovement : MonoBehaviour
 {
